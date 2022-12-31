a275ff7590a9c3d601b12db0b25c62e77ba25181
Kauê Rafael <94557995+Kaue0@users.noreply.github.com>
2022-12-31 06:12:48 -0300

    Questao 1

---
+++ Questao1/src/App/App.java
@@ -0,0 +1,17 @@
+package App;
+
+public class App {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Array a1 = new Array();
+
+        // Aqui eu chamo a função de colocar o array em ordem crescente //
+        a1.ordemCrescente();
+
+        // Aqui eu chamo a função de retornar o segundo maior número do array //
+        a1.segundoMaior();
+    }
+}
